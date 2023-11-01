82ae8b29a17b57a93b5bfcea7334c98b3ab8abc1
HarshHrs24 <107180900+HarshHrs24@users.noreply.github.com>
2023-11-02 02:35:29 +0530

    push

--- server.js
+++ server.js
@@ -1,22 +1,54 @@
-const express = require('express');
-const app = express();
+// import express from "express";
+import cors from 'cors'
 
-console.log("Hello World!");
-const LeetCode = require("leetcode-query");
+import {MongoClient} from "mongodb";
 
-const leetcode = new LeetCode.LeetCode();
+const app = express()
 
-async function func(){
-    for(let i=0 ;i<5000;i++){
-        console.log(i);
-        console.log("-----------------------------------------------------------------------------------------x-------------------------------------------------------------------------------------");
-        const user = await leetcode.user("kshatriyas");
-        console.log(user);
-    }
+app.use(express.json());
 
-}
+app.use(cors());
 
-func();
-app.listen(3000, () => {
-    console.log('Server is running on port 3000');
+
+
+const router = express.Router();
+
+
+
+router.post('/getusers', async (req, res) => {
+  try {
+    const uri = "mongodb+srv://testac042:qrBNlA5NMpGYDm3h@testleet.y9jtdbz.mongodb.net/?retryWrites=true&w=majority";
+
+    const client = new MongoClient(uri);
+
+    await client.connect();
+
+    const dbName = "student";
+    console.log("hello1")
+    const collectionName = "user";
+    console.log("hello2")
+    const database = client.db(dbName);
+    console.log("hello3")
+    // const collection = database.collection(collectionName);  
+    console.log("hello4")
+    const users = await  await database.collection(collectionName).find().toArray();
+    console.log(users)
+    res.json(users);
+    console.log("hello5")
+  } catch (err) {
+    res.status(500).json(err);
+    console.log(err)
+  }
 });
+
+app.use('/auth', router)
+
+// Create references to the database and collection in order to run
+// operations on them.
+
+
+
+
+const port = 3333
+app.listen(port, ()=> console.log(`Server Running on ${port}`));
+
